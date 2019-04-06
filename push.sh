fce93e622dc8698e326ef2e8ea1f96883b84e4b9
lazlo54 <32623331+lazlo54@users.noreply.github.com>
2019-04-06 21:52:19 +0300

    Create push.sh

---
+++ push.sh
@@ -0,0 +1,19 @@
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout master
+  git add _data *.pdf
+  git commit --message "[skip ci] Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/lazlo54/resume-cv.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages master 
+}
+
+setup_git
+commit_website_files
+upload_files
